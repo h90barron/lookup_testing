e02e450fb82ced8df67404d0d9d534e576b0fb67
hb90 <h90barron@gmail.com>
2017-04-11 20:50:45 +0000

    Remove log_search to test file permission errors

--- app/helpers/copiers_helper.rb
+++ app/helpers/copiers_helper.rb
@@ -1,11 +1,11 @@
 module CopiersHelper
   def log_search(input_params)
-    begin
-      f = File.open("search_log.txt", "a")
-      f.write(input_params.to_s + "\n")
-      f.close
-    rescue
-    end
+    # begin
+    #   f = File.open("search_log.txt", "a")
+    #   f.write(input_params.to_s + "\n")
+    #   f.close
+    # rescue
+    # end
   end
   
 end

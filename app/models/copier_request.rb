3d48b764e6b9fbc687c382c4ea37afcb1e2057d8
hb90 <h90barron@gmail.com>
2017-04-20 21:20:09 +0000

    Update about page and polish

--- app/models/copier_request.rb
+++ app/models/copier_request.rb
@@ -1,3 +1,3 @@
 class CopierRequest < ActiveRecord::Base
-  validates :req_name, :user_name, :user_email, :req_oem, presence: true
+  validates :req_name, :user_name, :user_email, :req_oem, :req_suppored_status, presence: true
 end
